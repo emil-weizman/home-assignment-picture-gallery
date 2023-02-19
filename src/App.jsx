6e9949173fb5469b67896d940393a47ce50e8a2b
emil-weizman <emil.weizman@gmail.com>
2023-02-20 00:00:18 +0200

    added vertical centering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-
 import { CardList } from './components/card-list/CardList'
 
 function App() {
   return (
-    <div>
+    <>
       <CardList />
-    </div>
+    </>
   )
 }
 
